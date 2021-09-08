985078f74a59575ded53d5088b5696e7a111d3da
Harrison Price <harrison.price@uqconnect.edu.au>
2021-09-08 20:35:23 +1000

    Added new multicast types to multicast_defs template

--- scripts/templates/multicast_defs.vhd
+++ scripts/templates/multicast_defs.vhd
@@ -8,11 +8,17 @@ use xil_defaultlib.packet_defs.all;
 package multicast_defs is 
 
     -- Constants
-    constant MULTICAST_CLUSTER_NODES    : integer := {{ multicastConfig.multicastClusterNodes }};
+    constant MULTICAST_GROUP_NODES  : integer := {{ multicastConfig.multicastClusterNodes }};
 
     -- Custom types
-    type t_MulticastClusterPackets is array (0 to (MULTICAST_CLUSTER_NODES-1)) of std_logic_vector((BUS_WIDTH-1) downto 0);
-    type t_MulticastClusterPacketsValid is array (0 to (MULTICAST_CLUSTER_NODES-1)) of std_logic;
+    type t_MulticastGroupPackets is array (0 to (MULTICAST_GROUP_NODES-1)) of std_logic_vector((BUS_WIDTH-1) downto 0);
+    type t_MulticastGroupPacketsValid is array (0 to (MULTICAST_GROUP_NODES-1)) of std_logic;
+    
+    type t_CombinedMulticastGroupPackets is array (1 to (MULTICAST_NETWORK_COLS), 1 to (MULTICAST_NETWORK_ROWS)) of t_MulticastGroupPackets;
+    type t_CombinedMulticastGroupPacketsValid is array (1 to (MULTICAST_NETWORK_COLS), 1 to (MULTICAST_NETWORK_ROWS)) of t_MulticastGroupPacketsValid;
+
+    type t_MulticastToMulticastPackets is array (1 to (MULTICAST_NETWORK_COLS), 1 to (MULTICAST_NETWORK_ROWS)) of std_logic_vector((BUS_WIDTH-1) downto 0);
+    type t_MulticastNetworkPacketsValid is array (1 to (MULTICAST_NETWORK_COLS), 1 to (MULTICAST_NETWORK_ROWS)) of std_logic;
 
 end package multicast_defs;
 
