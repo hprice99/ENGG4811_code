88bf696168b8d753c7bfedfb39588bcb4afdba74
Harrison Price <harrison.price@uqconnect.edu.au>
2021-09-13 21:26:29 +1000

    Generate the same matrix for all systems

--- scripts/MatrixConfig.py
+++ scripts/MatrixConfig.py
@@ -1,10 +1,33 @@
 import numpy as np
 import math
+import os
 
-# Matix configuration
+import os
+
+scriptLocation = os.path.realpath(__file__)
+scriptDirectory = os.path.dirname(scriptLocation)
+
+# Matrix configuration
 matrixSize = 16
+matrixFileName = "matrices.npz"
 
-A = np.random.randint(5, size=(matrixSize, matrixSize))
-B = np.random.randint(5, size=(matrixSize, matrixSize))
+def generate_matrices():
+    A = np.random.randint(5, size=(matrixSize, matrixSize))
+    B = np.random.randint(5, size=(matrixSize, matrixSize))
+
+    np.savez(matrixFileName, A=A, B=B)
+
+    return (A, B)
+
+if os.path.exists(matrixFileName):
+    matrices = np.load(matrixFileName)
+
+    A = matrices['A']
+    B = matrices['B']
+
+    if A.shape[0] != matrixSize or B.shape[0] != matrixSize or A.shape[0] != A.shape[1] or B.shape[0] != B.shape[1]:
+        (A, B) = generate_matrices()
+else:
+    (A, B) = generate_matrices()
 
 print(A @ B)
