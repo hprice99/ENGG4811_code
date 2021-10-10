d8043c0c435147fc141afad3a303f996b52113ae
Harrison Price <harrison.price@uqconnect.edu.au>
2021-10-10 11:52:37 +1000

    Multiplication timing test for 56x56 matrices

--- scripts/MatrixConfig.py
+++ scripts/MatrixConfig.py
@@ -8,7 +8,7 @@ scriptLocation = os.path.realpath(__file__)
 scriptDirectory = os.path.dirname(scriptLocation)
 
 # Matrix configuration
-matrixSize = 48
+matrixSize = 56
 matrixFileName = "matrices.npz"
 
 def generate_matrices():
