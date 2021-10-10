2b8964edc2507597068f3b22d1b46b0112755464
Harrison Price <harrison.price@uqconnect.edu.au>
2021-10-10 14:50:59 +1000

    Multiplication timing test for 72x72 matrices

--- firmware/fox_hoplite_multicast/matrix_config.h
+++ firmware/fox_hoplite_multicast/matrix_config.h
@@ -2,11 +2,11 @@
 #define MATRIX_CONFIG_H
 
 // Size of matrices used by each node
-#define FOX_MATRIX_SIZE     32
+#define FOX_MATRIX_SIZE     36
 #define MATRIX_SIZE         FOX_MATRIX_SIZE
 
 // Size of total matrix
-#define TOTAL_MATRIX_SIZE       64
+#define TOTAL_MATRIX_SIZE       72
 #define TOTAL_MATRIX_ELEMENTS   (TOTAL_MATRIX_SIZE * TOTAL_MATRIX_SIZE)
 
 #endif
